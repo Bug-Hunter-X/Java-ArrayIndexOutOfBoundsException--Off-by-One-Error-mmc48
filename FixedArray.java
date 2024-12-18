5e7bf465a3c18e193447be96534de655e2e3188f
BugHunter-X <askzenai@gmail.com>
2024-12-19 00:42:05 +0100

    Add FixedArray.java file

---
+++ FixedArray.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * 2;
+        }
+        // Accessing valid elements
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
